9bf3c3d29525c11fc34cd3c4d70c6e9ef492d164
wjx20000 <wjx20000@ly.com>
2018-03-07 04:09:06 +0800

    a

--- pages/delivery/reciever/index.js
+++ pages/delivery/reciever/index.js
@@ -1,65 +1,113 @@
 // pages/delivery/reciever/index.js
+const regexSpecial = /^(北京市|天津市|重庆市|上海市|香港特别行政区|澳门特别行政区)/;
+const regexProvince = /^(.*?(省|自治区))(.*?)$/;
+const regex = /^(.*?[市]|.*?地区|.*?特别行政区)(.*?[市区县])(.*?)$/g;
+let reciever = {
+  name: '',
+  mobile: '',
+  company: '',
+  province: '',
+  city: '',
+  region: '',
+  address: ''
+};
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    addressLine: ''
+    addressLine: '江苏省 苏州市 工业园区',
+    reciever: reciever
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.setNavigationBarTitle({ title: '新增收件人' });
+    wx.setNavigationBarTitle({ title: '收件人' });
   },
 
   /**
    * 省市区
    */
   bingAddressTap: function () {
-    var that = this;
+    let that = this;
     wx.chooseLocation({
       success: function (res) {
-        var regex = /^(北京市|天津市|重庆市|上海市|香港特别行政区|澳门特别行政区)/;
-        var REGION_PROVINCE = [];
-        var addressBean = {
-          REGION_PROVINCE: null,
-          REGION_COUNTRY: null,
-          REGION_CITY: null,
-          ADDRESS: null
-        };
+        var addressArray = [];
 
-        function regexAddressBean(address, addressBean) {
-          regex = /^(.*?[市州]|.*?地区|.*?特别行政区)(.*?[市区县])(.*?)$/g;
-          var addxress = regex.exec(address);
-          addressBean.REGION_CITY = addxress[1];
-          regexRegion(addxress[3], addressBean);
-          console.log(addxress);
+        function regexReciever(address, reciever) {
+          var _addressArray = regex.exec(address);
+          reciever.region = _addressArray[1];
+          reciever.city = _addressArray[2];
+          reciever.address = _addressArray[3] + "(" + res.name + ")";
+          console.log(_addressArray);
         }
 
-        function regexRegion(address, addressBean) {
-          regex = /^(.*?[区县])(.*?)$/g;
-          var addxress = regex.exec(address);
-          addressBean.REGION_COUNTRY = addxress[1];
-          addressBean.ADDRESS = addxress[2] + "(" + res.name + ")";
-          console.log(addxress);
-        }
-
-        if (!(REGION_PROVINCE = regex.exec(res.address))) {
-          regex = /^(.*?(省|自治区))(.*?)$/;
-          REGION_PROVINCE = regex.exec(res.address);
-          addressBean.REGION_PROVINCE = REGION_PROVINCE[1];
-          regexAddressBean(REGION_PROVINCE[3], addressBean);
+        if (!(addressArray = regexSpecial.exec(res.address))) {
+          addressArray = regexProvince.exec(res.address);
+          reciever.province = addressArray[1];
+          regexReciever(addressArray[3], reciever);
         } else {
-          addressBean.REGION_PROVINCE = REGION_PROVINCE[1];
-          regexAddressBean(res.address, addressBean);
+          reciever.province = addressArray[1];
+          regexReciever(res.address, reciever);
         }
-        that.setData(addressBean);
-        that.setData({ addressLine: addressBean.REGION_PROVINCE + " " + addressBean.REGION_CITY + " " + addressBean.REGION_COUNTRY });
+        that.setData({ reciever: reciever });
+        that.setData({ addressLine: reciever.province + " " + reciever.city + " " + reciever.region });
       }
     })
+  },
+
+  /**
+   * 省市区选择器级联
+   */
+  bindRegionChange: function (e) {
+    reciever.province = e.detail.value[0];
+    reciever.city = e.detail.value[1];
+    reciever.region = e.detail.value[2];
+    this.setData({
+      reciever: reciever,
+      addressLine: reciever.province + " " + reciever.city + " " + reciever.region
+    });
+  },
+
+  /**
+   * 添加寄件人
+   */
+  addReciever: function (e) {
+    var pages = getCurrentPages();
+    pages[pages.length - 2].setData({ reciever: this.data.reciever });
+    wx.navigateBack();
+  },
+
+  bindNameInput: function (e) {
+    reciever.name = e.detail.value;
+    this.setData({
+      reciever: reciever
+    });
+  },
+
+  bindMobileInput: function (e) {
+    reciever.mobile = e.detail.value;
+    this.setData({
+      reciever: reciever
+    });
+  },
+
+  bindCompanyInput: function (e) {
+    reciever.company = e.detail.value;
+    this.setData({
+      reciever: reciever
+    });
+  },
+
+  bindAddressInput: function (e) {
+    reciever.address = e.detail.value;
+    this.setData({
+      reciever: reciever
+    });
   },
 
   /**
--- pages/delivery/sender/index.js
+++ pages/delivery/sender/index.js
@@ -1,65 +1,114 @@
 // pages/delivery/sender/index.js
+const regexSpecial = /^(北京市|天津市|重庆市|上海市|香港特别行政区|澳门特别行政区)/;
+const regexProvince = /^(.*?(省|自治区))(.*?)$/;
+const regex = /^(.*?[市]|.*?地区|.*?特别行政区)(.*?[市区县])(.*?)$/g;
+let sender = {
+  name: '',
+  mobile: '',
+  company: '',
+  province: '',
+  city: '',
+  region: '',
+  address: ''
+};
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    addressLine: ''
+    addressLine: '江苏省 苏州市 工业园区',
+    sender: sender
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.setNavigationBarTitle({ title: '新增寄件人' });
+    wx.setNavigationBarTitle({ title: '寄件人' });
   },
 
   /**
    * 省市区
    */
   bingAddressTap: function () {
-    var that = this;
+    let that = this;
     wx.chooseLocation({
       success: function (res) {
-        var regex = /^(北京市|天津市|重庆市|上海市|香港特别行政区|澳门特别行政区)/;
-        var REGION_PROVINCE = [];
-        var addressBean = {
-          REGION_PROVINCE: null,
-          REGION_COUNTRY: null,
-          REGION_CITY: null,
-          ADDRESS: null
-        };
+        var addressArray = [];
+        
 
-        function regexAddressBean(address, addressBean) {
-          regex = /^(.*?[市州]|.*?地区|.*?特别行政区)(.*?[市区县])(.*?)$/g;
-          var addxress = regex.exec(address);
-          addressBean.REGION_CITY = addxress[1];
-          regexRegion(addxress[3], addressBean);
-          console.log(addxress);
+        function regexSender(address, sender) {
+          var _addressArray = regex.exec(address);
+          sender.region = _addressArray[1];
+          sender.city = _addressArray[2];
+          sender.address = _addressArray[3] + "(" + res.name + ")";
+          console.log(_addressArray);
         }
 
-        function regexRegion(address, addressBean) {
-          regex = /^(.*?[区县])(.*?)$/g;
-          var addxress = regex.exec(address);
-          addressBean.REGION_COUNTRY = addxress[1];
-          addressBean.ADDRESS = addxress[2] + "(" + res.name + ")";
-          console.log(addxress);
-        }
-
-        if (!(REGION_PROVINCE = regex.exec(res.address))) {
-          regex = /^(.*?(省|自治区))(.*?)$/;
-          REGION_PROVINCE = regex.exec(res.address);
-          addressBean.REGION_PROVINCE = REGION_PROVINCE[1];
-          regexAddressBean(REGION_PROVINCE[3], addressBean);
+        if (!(addressArray = regexSpecial.exec(res.address))) {
+          addressArray = regexProvince.exec(res.address);
+          sender.province = addressArray[1];
+          regexSender(addressArray[3], sender);
         } else {
-          addressBean.REGION_PROVINCE = REGION_PROVINCE[1];
-          regexAddressBean(res.address, addressBean);
+          sender.province = addressArray[1];
+          regexSender(res.address, sender);
         }
-        that.setData(addressBean);
-        that.setData({ addressLine: addressBean.REGION_PROVINCE + " " + addressBean.REGION_CITY + " " + addressBean.REGION_COUNTRY });
+        that.setData({ sender: sender });
+        that.setData({ addressLine: sender.province + " " + sender.city + " " + sender.region });
       }
     })
+  },
+
+  /**
+   * 省市区选择器级联
+   */
+  bindRegionChange: function (e) {
+    sender.province = e.detail.value[0];
+    sender.city = e.detail.value[1];
+    sender.region = e.detail.value[2];
+    this.setData({
+      sender: sender,
+      addressLine: sender.province + " " + sender.city + " " + sender.region
+    });
+  },
+
+  /**
+   * 添加寄件人
+   */
+  addSender: function (e) {
+    var pages = getCurrentPages();
+    pages[pages.length - 2].setData({ sender: this.data.sender });
+    wx.navigateBack();
+  },
+
+  bindNameInput: function (e) {
+    sender.name = e.detail.value;
+    this.setData({
+      sender: sender
+    });
+  },
+
+  bindMobileInput: function (e) {
+    sender.mobile = e.detail.value;
+    this.setData({
+      sender: sender
+    });
+  },
+
+  bindCompanyInput: function (e) {
+    sender.company = e.detail.value;
+    this.setData({
+      sender: sender
+    });
+  },
+
+  bindAddressInput: function (e) {
+    sender.address = e.detail.value;
+    this.setData({
+      sender: sender
+    });
   },
 
   /**
